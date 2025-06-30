101115e39980d00850708815bf6cf9fbff03aca0
muskanmanvi110 <muskanmanvi04@gmail.com>
2025-06-30 18:44:41 +0530

    Initial commit

---
+++ my-app/src/components/LoginSignup/LoginSignup.jsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './LoginSignup.css';
+
+import user_icon from '../Assets/person.png';
+import email_icon from '../Assets/email.png';
+import password_icon from '../Assets/password.png';
+
+const LoginSignup = () => {
+  const [action, setAction] = useState("SignUp");
+
+  return (
+    <div className='container'>
+      <div className="inputform">
+        <div className="title">{action}</div>
+        {action === "SignUp" && (
+          <div className="input">
+            <img src={user_icon} alt="" />
+            <input type="text" placeholder="Enter name" />
+          </div>
+        )}
+        <div className="input">
+          <img src={email_icon} alt="" />
+          <input type="text" placeholder='Enter email' />
+        </div>
+        <div className="input">
+          <img src={password_icon} alt="" />
+          <input type="password" placeholder='Enter password' />
+        </div>
+        {action === "Login" && (
+  <div className="forgotpassword">
+    <div className="text">Forgot password?</div>
+    <div className="click"><button>Click here</button></div>
+  </div>
+)}
+        <div className="buttons">
+          <div className="button">
+            <button onClick={() => setAction("Login")}>Login</button>
+          </div>
+          <div className="button">
+            <button onClick={() => setAction("SignUp")}>Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignup;
